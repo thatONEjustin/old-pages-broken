cc4bea39ae5d681204625df9ba13b343ea860169
thatONEjustin <justinpaelmo@gmail.com>
2023-07-16 01:47:40 -0500

    Tons of progress, close to merge and just outright replace main.
    Changes to be committed:
    	modified:   astro.config.mjs
    	modified:   package-lock.json
    	new file:   public/resume.pdf
    	new file:   src/components/pillButton.svelte
    	new file:   src/css/tailwind.css
    	new file:   src/css/tailwind/colors.css
    	new file:   src/css/tailwind/custom.css
    	new file:   src/css/theme.css
    	modified:   src/layouts/base.astro
    	new file:   src/layouts/footer.astro
    	modified:   src/layouts/header.astro
    	modified:   src/pages/index.astro
    	deleted:    src/pages/index.mdx

---
+++ src/components/pillButton.svelte
@@ -0,0 +1,40 @@
+<script>
+    export let href = '#';
+    export let target='_self';
+</script>
+
+<a href={href} target={target}>
+    <slot>Button Text</slot>
+</a>
+
+<style lang="css">
+a {
+    @apply py-3
+        px-5
+        bg-yellow-200
+        text-black
+        rounded-md
+        relative
+        overflow-hidden;
+}
+
+a::before {
+    content: '';
+
+    @apply bg-white
+        bg-opacity-25
+        w-3/5
+        h-full
+        top-0
+        left-[-125%]
+        absolute
+        pointer-events-none;
+
+    transform: skew(45deg);
+    transition: left 500ms ease-out;
+}
+
+a:hover::before {
+    @apply left-[125%];
+}
+</style>
