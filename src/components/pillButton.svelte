cae42f4b07019ee04be44e2adb418e0589c04e2b
thatONEjustin <2763180+thatONEjustin@users.noreply.github.com>
2024-01-14 20:13:41 -0600

    more work

--- src/components/pillButton.svelte
+++ src/components/pillButton.svelte
@@ -6,36 +6,3 @@
 <a {href} {target}>
   <slot>Button Text</slot>
 </a>
-
-<style lang="postcss">
-  a {
-    @apply py-3
-        px-5
-        bg-yellow-200
-        text-black
-        rounded-md
-        relative
-        overflow-hidden;
-  }
-
-  a::before {
-    content: "";
-
-    @apply bg-white
-        bg-opacity-25
-        w-3/5
-        h-full
-        top-0
-        left-[-125%]
-        absolute
-        pointer-events-none;
-
-    transform: skew(45deg);
-    transition: left 500ms ease-out;
-  }
-
-  a:hover::before {
-    @apply left-[125%];
-  }
-</style>
-
---
+++ src/js/data/menu.ts
@@ -0,0 +1,19 @@
+export type MenuItem = {
+  label: string,
+  url: string
+}
+
+export const menu_data: MenuItem[] = [
+  {
+    'label': 'home',
+    'url': '/'
+  },
+  {
+    'label': 'work',
+    'url': '/work'
+  },
+  {
+    'label': 'git',
+    'url': 'https://github.com/thatONEjustin'
+  }
+]
