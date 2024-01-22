cb30d7b89280d82fb52a06fbcf624db9c4e90bd1
thatONEjustin <2763180+thatONEjustin@users.noreply.github.com>
2024-01-21 18:59:44 -0600

    working build process hopefully

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
-import svelte from "@astrojs/svelte";
+// import svelte from "@astrojs/svelte";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://thatonejustin.github.io',
 
-  integrations: [tailwind({ applyBaseStyles: false, nesting: true }), mdx(), svelte(),]
+  integrations: [tailwind({ applyBaseStyles: false, nesting: true }), mdx(),]
 });
