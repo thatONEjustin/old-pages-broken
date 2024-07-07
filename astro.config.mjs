215f876b3cf921cf79c1d2d653841bb6b96d65a8
thatONEjustin <2763180+thatONEjustin@users.noreply.github.com>
2024-07-07 10:50:31 -0500

    proper astro alpine integration
    entrypoint file too! we can incorporate my own repos ^.^

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 // import svelte from "@astrojs/svelte";
 import mdx from "@astrojs/mdx";
 
+import alpinejs from "@astrojs/alpinejs";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://thatonejustin.github.io',
-
-  integrations: [tailwind({ applyBaseStyles: false, nesting: true }), mdx(),]
+  integrations: [tailwind({
+    applyBaseStyles: false,
+    nesting: true
+  }), mdx(), alpinejs({ entrypoint: '/src/js/main' })]
 });
