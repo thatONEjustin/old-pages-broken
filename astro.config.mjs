faa2ed138cb4a013c48c785c6efe35d6a4a593f2
thatONEjustin <2763180+thatONEjustin@users.noreply.github.com>
2024-07-19 21:46:18 -0500

    migrating into db content

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 // import svelte from "@astrojs/svelte";
 import mdx from "@astrojs/mdx";
-
 import alpinejs from "@astrojs/alpinejs";
+
+import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,6 +17,7 @@ export default defineConfig({
     mdx(),
     alpinejs({
       entrypoint: '/src/js/main'
-    })
+    }),
+    db()
   ]
 });
