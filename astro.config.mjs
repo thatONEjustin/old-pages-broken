c2d8daad6db40ad48b0a2f43a03c48f81b8113a7
Justin <2763180+thatONEjustin@users.noreply.github.com>
2024-07-20 14:57:26 -0500

    fancy button animation
    - component upgraded a lot
    
     Changes to be committed:
    	modified:   astro.config.mjs
    	modified:   bun.lockb
    	modified:   src/components/pill-button.astro
    	modified:   src/css/pages/about.css
    	modified:   src/css/tailwind/layers/base.css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
+
 import tailwind from "@astrojs/tailwind";
-// import svelte from "@astrojs/svelte";
 import mdx from "@astrojs/mdx";
 import alpinejs from "@astrojs/alpinejs";
-
-import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
