a9b1549e1ffb80dfb159857f8e561cf8e772935b
thatONEjustin <2763180+thatONEjustin@users.noreply.github.com>
2024-07-24 13:28:24 -0500

    goto top component

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,5 +1,8 @@
 import type { Alpine } from 'alpinejs'
 
+import intersect from '@alpinejs/intersect'
+
 export default (Alpine: Alpine) => {
   console.log('Alpine!')
+  Alpine.plugin(intersect)
 }
