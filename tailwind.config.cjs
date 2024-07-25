ba7a6225c98d04039890a8915d74b8bd9024bbe0
thatONEjustin <2763180+thatONEjustin@users.noreply.github.com>
2024-07-24 22:38:10 -0500

    minor

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-// const colors = require('tailwindcss/colors')
 
 module.exports = {
   content: ['./src/**/*.{astro,html,ts,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     fontFamily: {
-      // 'sans': ['Ubuntu', 'sans-serif'],
-      // 'serif': ['"Delicious Handrawn"', 'serif'],
-      // 'nabla': ['Nabla', '"Delicious Handrawn"', 'sans-serif'],
-      // 'caveat': ['Caveat', 'serif']
       'nf-symbols': ['Symbols Nerd Font'],
       'belanosima': ['belanosima', 'serif']
     },
