94dea6f22e3d53e14a8733453111cb9436d563c2
thatONEjustin <2763180+thatONEjustin@users.noreply.github.com>
2024-09-30 22:23:15 -0500

    minor edits and updates to versions

--- src/js/alpine.ts
+++ src/js/alpine.ts
@@ -2,7 +2,11 @@ import type { Alpine } from 'alpinejs'
 
 import intersect from '@alpinejs/intersect'
 
+import slideshow from './slideshow'
+
 export default (Alpine: Alpine) => {
   console.log('Alpine!')
+
+  Alpine.plugin(slideshow)
   Alpine.plugin(intersect)
 }
